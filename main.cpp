312654dc08b6aef6813d0819d171fc77554b0921
Noah <nkempers@outlook.de>
2021-01-11 09:53:33 +0000

    first test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+using namespace std; 
 
 int main() {
-  std::cout << "Hello World!\n";
+    float pi = 3.1415; //int
+    float durchmesser = 0.7;
+    float hoehe = 1.5;
+    float vol = 10;
+    pi = 0.7;
+    char anzahl; 
+    int vol_glas = pi * (durchmesser * durchmesser)/4 * hoehe; 
+    anzahl = vol / vol_glas;
+     cout << vol_glas << "##" << anzahl << " Glaeser koennen gefuellt werden.";
+     return 0;
 }
